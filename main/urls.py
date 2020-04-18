7a229de46c7def5205591f643deb3a0ae635d6fe
Emma Hobday <emma.hobday@gmail.com>
2020-04-18 14:33:48 +0100

    updated backend search functions

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from main.views.recipe import AllRecipesListView
+from main.views.recipe import AllRecipesListView, FilteredRecipeView, FridgeRecipeView
 
 urlpatterns = [
-  path('recipes/', AllRecipesListView.as_view())
+  path('recipes/', AllRecipesListView.as_view()),
+  path('recipes/type/<str:filter>', FilteredRecipeView.as_view()),
+  path('recipes/fridge/<str:query>', FridgeRecipeView.as_view())
 ]
--- main/views/recipe.py
+++ main/views/recipe.py
@@ -26,4 +26,46 @@ class AllRecipesListView(ListCreateAPIView):
       serializer = BasicRecipeSerializer(recipes, many=True)
       return self.get_paginated_response(serializer.data)
 
+# this returns recipes that have main_protein that matches the search filter taken from the URL of the GET request
+class FilteredRecipeView(ListCreateAPIView):
+  serializer_class = BasicRecipeSerializer
+  pagination_class = AllRecipesPagination 
+
+  def get(self, request, filter): 
+      recipes = self.paginate_queryset(Recipe.objects.filter(main_protein__iexact=filter))
+      serializer = BasicRecipeSerializer(recipes, many=True)
+      return self.get_paginated_response(serializer.data)
+
+
+# this takes up to 3 search terms from the URL of the GET request (named query) and returns recipes that include these search terms in the ingredients list
+class FridgeRecipeView(ListCreateAPIView):
+  # queryset = Recipe.objects.filter(ingredients_lines__icontains='')
+  serializer_class = BasicRecipeSerializer
+  pagination_class = AllRecipesPagination 
+
+  def get(self, request, query): 
+      if query.count('&') == 2:
+        query1 = query.split('&')[0]
+        query2 = query.split('&')[1]
+        query3 = query.split('&')[2]
+      elif query.count('&') == 1:
+        query1 = query.split('&')[0]
+        query2 = query.split('&')[1]
+        query3 = ''
+      else:
+        query1 = query
+        query2 = ''
+        query3 = ''
+
+      recipes = self.paginate_queryset(Recipe.objects.filter(
+        ingredients_lines__icontains=query1
+        ).filter(
+          ingredients_lines__icontains=query2
+        ).filter(
+          ingredients_lines__icontains=query3
+        ))
+      serializer = BasicRecipeSerializer(recipes, many=True)
+      return self.get_paginated_response(serializer.data)
+    
+
   
